838785c1757fec62f7447a8acd18b645eb063f04
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-11-20 18:13:46 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const foods = ["pizza", "burger", "fingerChips", "donuts", "springRoll"];
+const numberArray = [12,324,213,4,2,3,45,4234];
+const myArray = [11, 34, 20, 5, 53, 16];
+
+// Progression 1:
+
+
+// Progression 2:
+
+
+// Progression 3:
+
+
+// Progression 4:
+
+
+// Progression 5:
+
+
+// Progression 6:
+
+
+// Progression 7:
+
+
